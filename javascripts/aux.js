6fac9b7a18240581b54f1aaf747264b0877e2461
DerekAlmanza <DerekAlmanza@ciencias.unam.mx>
2020-07-24 14:13:55 -0500

    Juego terminado

--- javascripts/aux.js
+++ javascripts/aux.js
@@ -2,8 +2,30 @@ let getRandomNumber = limit => {
     return Math.floor(Math.random() * limit);
 }
 
-let getDistance = (click, target) => {
-    let diffx = click.offsetX - target.x;
-    let diffy = click.offsetY - target.y;
-    return Math.sqrt(Math.pow)
+let getDistance = (userClick, target) => {
+    let diffx = userClick.offsetX - target.x;
+    let diffy = userClick.offsetY - target.y;
+    return Math.sqrt(Math.pow(diffx, 2) + Math.pow(diffy, 2));
 }
+
+let hintDistance = distance => 
+    distance < 20 ? "You find the treasure!"
+    : distance < 30 ? "Boiling Hot" 
+    : distance < 40 ? " Really Hot"
+    : distance < 60 ? "Hot"
+    : distance < 100 ? "Warm"
+    : distance < 150 ? "Cold"
+    : distance < 200 ? "Really Cold"
+    : "Freezing";
+
+//let levelMap = {
+//    switch (key) {
+//        case value:
+//            
+//            break;
+//    
+//        default:
+//            break;
+//    }
+//}
+
--- javascripts/game.js
+++ javascripts/game.js
@@ -1,2 +1,26 @@
-let width = 400;
-let height = 400;
+const width = 300;
+const height = 300;
+
+let whereIsTheTarget = {
+    x: getRandomNumber(width),
+    y: getRandomNumber(height)
+};
+
+console.log(whereIsTheTarget);
+
+let $map = document.getElementById("map");
+let $distance = document.getElementById("distance");
+var numberOfClicks = 0;
+
+$map.addEventListener("click", function(userClick) {
+    numberOfClicks ++;
+    let distance = getDistance(userClick, whereIsTheTarget);
+    let howCloseAmI = hintDistance(distance);
+    console.log(howCloseAmI);
+    $distance.innerHTML = `<h1>${howCloseAmI}<h1>`;
+    
+    if (distance < 20) {
+        alert(`You find the treasure in ${numberOfClicks} clicks!`),
+        location.reload();
+    };
+})
